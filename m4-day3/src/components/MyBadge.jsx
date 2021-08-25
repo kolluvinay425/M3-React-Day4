927125cb5980757be0a38f096545322cd9725189
kolluvinay425 <kolluvinay425@gmail.com>
2021-08-25 17:01:55 +0200

    Update MyBadge.jsx

---
+++ m4-day3/src/components/MyBadge.jsx
@@ -0,0 +1,19 @@
+
+import { Button,Form,Badge} from 'react-bootstrap'
+
+const MyBadge=({text,color}) =>{
+    
+    
+        return (
+            <div className="text-center">
+                <h2>Make A Badge :)</h2>
+                <Button variant={color}>
+                     {text} <Badge variant={color}></Badge>
+                    <span className="sr-only">unread messages</span>
+                </Button>
+            </div>
+        )
+    
+        }
+
+export default MyBadge
