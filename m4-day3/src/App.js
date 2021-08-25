3ee620fbff2a718497774065ba6dee46348b30bb
kolluvinay425 <kolluvinay425@gmail.com>
2021-08-25 16:56:45 +0200

    update

--- m4-day3/src/App.js
+++ m4-day3/src/App.js
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import WarningSign from "./components/WarningSign";
+import SingleBook from "./components/SingleBook";
 
 function App() {
   return (
     <div>
       <WarningSign text="this is the text" />;
+      <SingleBook />
     </div>
   );
 }
--- m4-day3/src/components/WarningSign.jsx
+++ m4-day3/src/components/WarningSign.jsx
@@ -1,42 +1,19 @@
-import Navbar from 'react-bootstrap/Navbar'
+
+import {Navbar} from 'react-bootstrap'
 
 
 const WarningSign = ({text})=>{
     return(
         <>
         
-  <Navbar bg="light">
-    <Navbar.Brand href="#home">{text}</Navbar.Brand>
-  </Navbar>
-  <br />
-  <Navbar bg="light">
-    <Navbar.Brand>Brand text</Navbar.Brand>
-  </Navbar>
-  <br />
+  
   <Navbar bg="dark">
-    <Navbar.Brand href="#home">
-      <img
-        src="/logo.svg"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-        alt="React Bootstrap logo"
-      />
-    </Navbar.Brand>
+    <Navbar.Brand  >{text}</Navbar.Brand>
   </Navbar>
-  <br />
-  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">
-      <img
-        alt=""
-        src="/logo.svg"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-      />{' '}
-      React Bootstrap
-    </Navbar.Brand>
-  </Navbar>
+  
+ 
+   
+   
 </>
         
     )
